1c4789aa95029f1a46adac6a79c8e12e9538c764
Mario <mariojt15@gmail.com>
2026-01-13 16:18:42 -0300

    feat: create StyledButton components

---
+++ src/components/Styled/StyledButton.tsx
@@ -0,0 +1,31 @@
+import styled from "@emotion/styled";
+import type { ReactNode } from "react";
+import type React from "react";
+interface MyButtonProps {
+  children: ReactNode;
+}
+
+const StyledButton = styled("button")(({ theme }) => ({
+  backgroundColor: "transparent",
+  border: `1px solid ${theme.palette.primary.contrastText}`,
+  borderRadius: "3px",
+  padding: "5px 15px",
+  width: "100%",
+  color: theme.palette.primary.contrastText,
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "10px",
+  "&:hover": {
+    backgroundColor: theme.palette.secondary.light,
+  },
+}));
+const MyButton: React.FC<MyButtonProps> = ({ children }) => {
+  return (
+    <>
+      <StyledButton>{children}</StyledButton>
+    </>
+  );
+};
+
+export default MyButton;
